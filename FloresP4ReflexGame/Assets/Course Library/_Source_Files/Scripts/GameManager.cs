038683a324fc4ea66e617f9ce8f464e2475e6459
maddi-stinks <740546@student.dusd.net>
2026-05-04 11:52:08 -0700

    started lesson 1
    started project

---
+++ FloresP4ReflexGame/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public float spawnRate = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(spawnTarget());
+
+    }
+
+    IEnumerator spawnTarget()
+    {
+        while (true)
+        { 
+        yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);   
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
