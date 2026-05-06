25c9fd17110d73b216065ff375a4337d5bd4884e
maddi-stinks <740546@student.dusd.net>
2026-05-06 11:32:34 -0700

    2 lessons finished
    almst done

--- FloresP4ReflexGame/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
+++ FloresP4ReflexGame/Assets/Course Library/_Source_Files/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
     public float spawnRate = 1.0f;
+    private int score;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(spawnTarget());
+        score = 0;
+        UpdateScore(0);
 
     }
 
@@ -22,6 +27,12 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);   
         }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
